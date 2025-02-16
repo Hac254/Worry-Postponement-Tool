24510c727651d22e02805971c777d7bf55c90e8a
Hac254 <wafulajoosh@gmail.com>
2025-02-16 14:45:14 +0300

    fisr

--- postcss.config.js
+++ postcss.config.js
@@ -1,6 +1,19 @@
+
 export default {
   plugins: {
     tailwindcss: {},
     autoprefixer: {},
   },
+  
+    /* config options here */
+    eslint: {
+      // Warning: This allows production builds to successfully complete even if
+      // your project has ESLint errors.
+      ignoreDuringBuilds: true,
+    },
+    images: {
+      domains: ['i.ibb.co','telegra.ph'],
+    },
+    
+  
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -8,4 +8,15 @@ export default {
     extend: {},
   },
   plugins: [],
+    /* config options here */
+    eslint: {
+      // Warning: This allows production builds to successfully complete even if
+      // your project has ESLint errors.
+      ignoreDuringBuilds: true,
+    },
+    images: {
+      domains: ['i.ibb.co','telegra.ph'],
+    },
+    
+  
 }
